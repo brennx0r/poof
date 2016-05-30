a7d866d00294f3d18c39533e62e6074a2e0aeeb2
brennx0r <brennx0r@gmail.com>
2016-05-30 13:54:08 -0700

    Add age to the mix! Also, add Dwarf names.

--- character_generator.rb
+++ character_generator.rb
@@ -13,19 +13,40 @@
     def character_name(character_gender)
 		nameArray = IO.readlines(character_gender.to_s+"_names.txt")
 		randomize_array = rand(0..10)
-		return nameArray[randomize_array].chomp	
+		return nameArray[randomize_array].chomp.to_s
     end
 
 	def character_race()
 		raceArray = IO.readlines("race.txt")
 		randomize_array = rand(0..8)
-		return raceArray[randomize_array].chomp
+		return raceArray[randomize_array].chomp.to_s
+	end
+
+	def character_age(character_race)
+		if character_race.to_s == "Dwarf"
+			return rand(50..350).to_s
+		#	
+		# The Elf case isn't going to match anything for now 
+		# Will need to write up Issue about this.	
+		#
+		elsif character_race.to_s == "Elf"
+			return rand(100..750).to_s	
+		elsif character_race.to_s == "Halfling"
+			return rand(20..250).to_s
+		elsif character_race.to_s == "Gnome"
+			return rand(30..500).to_s
+		elsif character_race.to_s == "Half-Elf"
+			return rand(20..180).to_s
+		else
+			# Return age values applicable to Humans, Dragonborn, Half-Orc, Tiefling, 
+			return rand(15..80).to_s
+		end
 	end
 
 	def character_class()
 		classArray = IO.readlines("class.txt")
 		randomize_array = rand(0..7)
-		return classArray[randomize_array].chomp
+		return classArray[randomize_array].chomp.to_s
 	end
 
 puts "What is your character's desired gender? [Valid options: M, F, or NB]"
@@ -35,5 +56,37 @@ g = character_gender
 n = character_name(character_gender)
 r = character_race
 c = character_class
+a = character_age(character_race)
 
-puts "You are "+n.to_s+", the "+g.to_s+" "+r.to_s+" "+c.to_s+"!"
+puts ""
+puts ""
+puts ""
+puts "You are "+n+", the "+g+" "+r+" "+c+"!"
+puts ""
+puts "------------------------------------------------------------"
+puts ""
+puts "Vital Statistics"
+puts "------------------------------------------------------------"
+puts "Height: "
+puts "Weight: "
+puts "Age: "+a
+puts "Speed: "
+puts ""
+puts "------------------------------------------------------------"
+puts "Backstory"
+puts "------------------------------------------------------------"
+puts "Ideals: "
+puts "Bonds: "
+puts "Flaws: "
+puts ""
+puts "------------------------------------------------------------"
+puts "Abilities"
+puts "------------------------------------------------------------"
+puts "Strength: "
+puts "Dexterity: "
+puts "Constitution: "
+puts "Intelligence: "
+puts "Wisdom: "
+puts "Charisma: "
+puts "------------------------------------------------------------"
+
