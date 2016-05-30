1f1267ed456a65cdff29522095565dc2e8353cf4
brennx0r <brennx0r@gmail.com>
2016-05-30 15:58:16 -0700

    Add alignment and background for a character

--- character_generator.rb
+++ character_generator.rb
@@ -98,6 +98,18 @@
 		return classArray[randomize_array].chomp.to_s
 	end
 
+	def character_alignment()
+		alignmentArray = IO.readlines("alignment.txt")
+		randomize_array = rand(0..8)
+		return alignmentArray[randomize_array].chomp.to_s
+	end
+
+	def character_background()
+		backgroundArray = IO.readlines("background.txt")
+		randomize_array = rand(0..12)
+		return backgroundArray[randomize_array].chomp.to_s
+	end
+
 	def roll_ability()
 		rollArray = []
 		4.times {
@@ -119,10 +131,12 @@ g = character_gender
 n = character_name(character_gender)
 r = character_race
 c = character_class
-a = character_age(character_race)
+age = character_age(character_race)
 h = character_height(character_race)
 w = character_weight(character_race)
 s = character_speed(character_race)
+align = character_alignment
+b = character_background
 str = roll_ability.to_s
 dex = roll_ability.to_s
 con = roll_ability.to_s
@@ -137,14 +151,16 @@ puts "You are "+n+", the "+g+" "+r+" "+c+"!"
 puts "------------------------------------------------------------"
 puts "Vital Statistics"
 puts "------------------------------------------------------------"
-puts "Height:  "+h
-puts "Weight:  "+w
-puts "Age:     "+a
-puts "Speed:   "+s
+puts "Height:    "+h
+puts "Weight:    "+w
+puts "Age:       "+age
+puts "Speed:     "+s
+puts "Alignment: "+align
 puts ""
 puts "------------------------------------------------------------"
 puts "Backstory"
 puts "------------------------------------------------------------"
+puts "Background: "+b
 puts "Ideals: "
 puts "Bonds: "
 puts "Flaws: "
